01d4dfd6f76360a3987412f4708241d49fd50643
Luke Yang <luyang@redhat.com>
2023-08-28 13:19:59 -0400

    wip

--- osbuild/util/ostree.py
+++ osbuild/util/ostree.py
@@ -160,6 +160,7 @@ def deployment_path(root: PathLike, osname: str, ref: str, serial: int):
 
     repo = os.path.join(base, "repo")
     stateroot = os.path.join(base, "deploy", osname)
+    stateroot = base
 
     # commit = rev_parse(repo, ref)
     # skopeo inspect containers-storage:[] | jq 
