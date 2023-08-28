a70bd64220310be20e43421b15cc7f71c18c7cf5
Luke Yang <luyang@redhat.com>
2023-08-28 09:40:57 -0400

    wip

--- osbuild/util/ostree.py
+++ osbuild/util/ostree.py
@@ -163,7 +163,7 @@ def deployment_path(root: PathLike, osname: str, ref: str, serial: int):
 
     # commit = rev_parse(repo, ref)
     # skopeo inspect containers-storage:[] | jq 
-    r = subprocess.run(["skopeo", "inspect", "containers-storage:[overlay@/run/osbuild/tree/usr/share/containers/storage+/run/containers/storage]localhost/fcos"],
+    r = subprocess.run(["skopeo", "inspect", "containers-storage:[overlay@/run/osbuild/tree/usr/share/containers/storage+/run/containers/storage]localhost/fcos", "|", "jq", "'.Labels[\"ostree.commit\"]'"],
                        encoding="utf8",
                        stdout=subprocess.PIPE,
                        stderr=subprocess.STDOUT,
